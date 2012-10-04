1305af3fa2793bee9380863b4ff0366ebfad0ba2
flyswatter <somniac@me.com>
2012-10-04 14:47:46 -0700

    Created iterative functions, except workshops so far.

--- src.js
+++ src.js
@@ -9,8 +9,8 @@ function readyFunction(){
 			"Tribe F", "Tribe G", "Tribe H",
 			"Tribe I", "Tribe J"];
 		var workshops = [1,2,3,4,5];
-		var days = ["monday","tuesday","wednesday","thursday","friday"];
-		var chores = ["breakfastKP", "lunchKP", "dinnerKP", "johnDuty"];
+		var days = ["sunday", "monday","tuesday","wednesday","thursday","friday"];
+		var chores = ["KP", "johnDuty", "campfire"];
 
 		var criteria1 = function(schedule){
 			return true;
@@ -21,32 +21,101 @@ function readyFunction(){
 		var criteriaToMeet = [criteria1, criteria2];
 
 		function checkCriteria(schedule){
-			for (var i=0; i < criteriaToMeet.length; i++){
-				if (criteriaToMeet[i](schedule)===false){
+			criteriaToMeet.forEach(criteria){
+				if (criteria(schedule)===false){
 					return false;
 				}
 				return true;
 			}
 		}
 
-		function generateSchedule(tribes){
-			var schedule = {};
-			for (day in days){
-				schedule[day]={};
+		var sampleDaySchedule = [
+			{name:"monday",
+			kp:["Tribe A", "Tribe F"],
+			johnDuty:["Tribe B", "Tribe C"],
+			campFire:["Tribe D", "Tribe E"],
+			workshops:{
+				1:["Tribe J", "Tribe I"],
+				2:["Tribe C", "Tribe A"],
+				3:["Tribe B", "Tribe F"],
+				4:["Tribe D", "Tribe J"],
+				5:["Tribe E", "Tribe G"]
 			}
-			for (day in schedule){
-				for (chore in chores){
-					//I have no idea how you'll systematically
-					//generate this many variables.  I look
-					//Forward to seeing it done nicely :)
+		}];
+
+		var permArr = [], usedChars = [], chorePermutations = [], workshopPermutations=[];
+		function permute(input) {
+		    var i, ch;
+		    for (i = 0; i < input.length; i++) {
+		        ch = input.splice(i, 1)[0];
+		        usedChars.push(ch);
+		        if (input.length == 0) {
+		            permArr.push(usedChars.slice());
+		        }
+		        permute(input);
+		        input.splice(i, 0, ch);
+		        usedChars.pop();
+		    }
+		    return permArr
+		};
+		var schedule=[{},{},{},{},{},{}];
+
+		function generateSchedules(){
+			var schedules = [];
+
+			//First generate all chore permutations:
+			var singleChorePermutations = permute(tribes);
+
+
+			//Go through all KP permutations:
+			singleChorePermutations.forEach(kpPermutation){
+				var kpNumber = 0;
+				//Add this permutation to the current schedule;
+				for (var i = 0; i < days.length; i++){
+					schedule[i][name]=days[i];
+					//Sunday has no KP
+					if (days[i] !== "sunday") {
+						schedule[i][kp].push(kpPermutation[kpNumber]);
+						kpNumber+=1;
+						schedule[i][kp].push(kpPermutation[kpNumber]);
+						kpNumber+=1;
+					}
 				}
+				//Next we'll go through all John Duty permutations:
+				singleChorePermutations.forEach(jdPermutation){
+					var jdNumber = 0;
+					//Add this permutation to the current schedule;
+					for (var i = 0; i < days.length; i++){
+						//Sunday has no jd
+						if (days[i] !== "sunday") {
+							schedule[i][jd].push(jdPermutation[jdNumber]);
+							jdNumber+=1;
+							schedule[i][jd].push(jdPermutation[jdNumber]);
+							jdNumber+=1;
+						}
+					}
+					//Next we'll go through all Campfire permutations:
+					singleChorePermutations.forEach(cfPermutation){
+						var cfNumber = 0;
+						//Add this permutation to the current schedule;
+						for (var i = 0; i < days.length; i++){
+							//Wednesday has no cf
+							if (days[i] !== "wednesday") {
+								schedule[i][cf].push(cfPermutation[cfNumber]);
+								cfNumber+=1;
+								schedule[i][cf].push(cfPermutation[cfNumber]);
+								cfNumber+=1;
+							}
+						}
+
+						//Last we'll go through all workshop permutations.
+
+					}
+				}
+
 			}
-			//Generate schedule, run it through checkCriteria.
-			//If checkCriteria returns true, then that schedule
-			//meets all criteria functions.			
+			return schedules;	
 		}
-
-
-		var schedule = generateSchedule(tribes);
+		var schedules = generateSchedules();
 	});
 }
