fc105afc209de9f9026efabab636bd6df6c69c40
flyswatter <somniac@me.com>
2012-10-04 15:07:12 -0700

    Workshop generation created.

--- src.js
+++ src.js
@@ -34,13 +34,13 @@ function readyFunction(){
 			kp:["Tribe A", "Tribe F"],
 			johnDuty:["Tribe B", "Tribe C"],
 			campFire:["Tribe D", "Tribe E"],
-			workshops:{
+			workshops:[{
 				1:["Tribe J", "Tribe I"],
 				2:["Tribe C", "Tribe A"],
 				3:["Tribe B", "Tribe F"],
 				4:["Tribe D", "Tribe J"],
 				5:["Tribe E", "Tribe G"]
-			}
+			}]
 		}];
 
 		var permArr = [], usedChars = [], chorePermutations = [], workshopPermutations=[];
@@ -109,7 +109,45 @@ function readyFunction(){
 						}
 
 						//Last we'll go through all workshop permutations.
-
+						singleChorePermutations.forEach(w1Permutation){
+							singleChorePermutations.forEach(w2Permutation){
+								singleChorePermutations.forEach(w3Permutation){
+									singleChorePermutations.forEach(w4Permutation){
+										singleChorePermutations.forEach(w5Permutation){
+											var workshops = [w1Permutation,w2Permutation,w3Permutation,w4Permutation,w5Permutation];
+											function addWorkshop(wPermutation){
+												return {
+													1:[wPermutation[0], wPermutation[1]],
+													2:[wPermutation[2], wPermutation[3]],
+													3:[wPermutation[4], wPermutation[5]],
+													4:[wPermutation[6], wPermutation[7]],
+													5:[wPermutation[8], wPermutation[9]]
+												};
+											}
+											var workshopNumber = 0;
+											schedule.forEach(day){
+												day[workshops]=[];
+												if (day.name === "monday" ||  day.name === "tuesday"||day.name === "monday"||day.name === "thursday") {
+													day.workshops.push(addWorkshop(workshops[workshopNumber]));
+													workshopNumber+=1;
+												}
+												if (day.name === "wednesday") {
+													day.workshops.push(addWorkshop(workshops[workshopNumber]));
+													workshopNumber+=1;
+													day.workshops.push(addWorkshop(workshops[workshopNumber]));
+													workshopNumber+=1;
+												}
+											}
+											//All workshops have been added to the schedule.  Time to evaluate it, and add it to the list if it's good.
+											if (checkCriteria(schedule)) {
+												schedules.push(schedule);
+												console.log("Good schedule found!: "+schedule);
+											};
+										}
+									}
+								}
+							}
+						}
 					}
 				}
 
